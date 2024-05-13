ed4cb741c2deae8b84497d72664c89325305f55e
Manh Nguyen <manhnd9293@gmail.com>
2024-05-13 11:20:44 +0700

    test tinymce

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,15 +1,22 @@
 import PageTitle from '@/pages/common/PageTitle.tsx';
-import { DataTableDemo } from '@/pages/test/table/test-full-data-table/TestFullDataTable.tsx';
-
+import EditorComponent from '@/pages/test/editor/EditorComponent.tsx';
+import { useState } from 'react';
+import DOMPurify from 'dompurify';
 
 function TestPage() {
-
+  const [textContent, setTextContent] = useState('');
   return (
     <div>
-      <PageTitle>Test Pages</PageTitle>
-
-      <div className="container mx-auto mt-4">
-        <DataTableDemo/>
+      <PageTitle>Test Page</PageTitle>
+      <div className={'mt-8'}>
+        <EditorComponent content={textContent}
+                         onUpdateContent={setTextContent}/>
+      </div>
+      <div className={'mt-8'}>
+        <div className={'text-xl'}>Content render</div>
+        <div>
+          <div className={'prose prose-a:text-blue-600 max-w-none'} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(textContent)}}></div>
+        </div>
       </div>
     </div>
 
