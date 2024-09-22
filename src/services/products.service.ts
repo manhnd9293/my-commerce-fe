b034404021d67c4a203970b37b163c6194656f81
Manh Nguyen <manhnd9293@gmail.com>
2024-09-22 15:34:58 +0700

    feat: select product by category

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,24 +1,31 @@
-import httpClient from '@/http-client/http-client.ts';
-import { Product } from '@/dto/product/product.ts';
-import { z } from 'zod';
-import { productFormSchema } from '@/pages/admin/products/form/ProductForm.tsx';
+import httpClient from "@/http-client/http-client.ts";
+import { Product } from "@/dto/product/product.ts";
+import { z } from "zod";
+import { productFormSchema } from "@/pages/admin/products/form/ProductForm.tsx";
+import { ProductQueryDto } from "@/dto/product/product-query.dto.ts";
 
 class ProductsService {
   async create(data: z.infer<typeof productFormSchema>) {
-    const product = await httpClient.post('/products', data) as Product;
+    const product = (await httpClient.post("/products", data)) as Product;
     const images = data.newImages;
     const formData = new FormData();
 
-    images && Array.from(images).forEach(item => formData.append('productImages', item));
+    images &&
+      Array.from(images).forEach((item) =>
+        formData.append("productImages", item),
+      );
     return httpClient.patch(`/products/${product.id}/images`, formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
+        "Content-Type": "multipart/form-data",
+      },
+    });
   }
 
-  getAll(): Promise<Product[]> {
-    return httpClient.get('/products');
+  getAll(productQueryDto: ProductQueryDto): Promise<Product[]> {
+    let queryString = "";
+    const { categoryId } = productQueryDto;
+    if (categoryId) queryString += `categoryId=${categoryId}`;
+    return httpClient.get(`/products?${queryString}`);
   }
 
   get(id: string): Promise<Product | null> {
@@ -26,17 +33,19 @@ class ProductsService {
   }
 
   async update(updateProduct: z.infer<typeof productFormSchema>) {
-
-    await httpClient.put(`/products`, updateProduct)
+    await httpClient.put(`/products`, updateProduct);
 
     const images = updateProduct.newImages;
     const formData = new FormData();
-    images && Array.from(images).forEach(item => formData.append('productImages', item));
+    images &&
+      Array.from(images).forEach((item) =>
+        formData.append("productImages", item),
+      );
     return httpClient.patch(`/products/${updateProduct.id}/images`, formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
+        "Content-Type": "multipart/form-data",
+      },
+    });
   }
 }
 
