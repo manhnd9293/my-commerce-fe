a60e388e51b46a12c95b95e67994a73e1947a771
Manh Nguyen <manhnd9293@gmail.com>
2025-02-03 18:05:30 +0700

    build: fix server env config

--- src/http-client/http-client.ts
+++ src/http-client/http-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}`,
 });
 
 httpClient.interceptors.request.use(
